664e8c25c6c1defaf5c80dfee6fbbf6f5fb77158
1KS20AI001-Abhishek <94467192+1KS20AI001-Abhishek@users.noreply.github.com>
2022-04-02 12:01:16 +0530

    Update Lab_prog07.c

--- Lab_prog07.c
+++ Lab_prog07.c
@@ -1,151 +1,185 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h> 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int count=0; 
+typedef enum Menu {CH_EXIT, CH_CREATE, CH_DISPLAY, CH_INSERT_FRONT, CH_DELETE_FRONT, CH_INSERT_END, CH_DELETE_END} menu_t;
 
-struct node { 
-  int sem,phno; 
-  char name[20],branch[10],usn[20]; 
-  struct node *next; 
-} *first=NULL,*last=NULL,*temp=NULL,*temp1; 
-
-void create() { 
-  int sem,phno; 
-  char name[20],branch[10],usn[20]; 
-  temp=(struct node*)malloc(sizeof(struct node)); 
-  printf("\n Enter usn,name, branch, sem, phno of student : "); 
-  scanf("%s %s %s %d %d", usn, name,branch, &sem,&phno); 
-  strcpy(temp->usn,usn); 
-  strcpy(temp->name,name); 
-  strcpy(temp->branch,branch); 
-  temp->sem = sem; 
-  temp->phno = phno; 
-  temp->next=NULL; 
-  count++; 
+typedef struct listnode {
+  char USN[10];
+  char name[50];
+  char programme[10];
+  char sem[5];
+  char phone_no[10];
+  struct listnode* next;
+} listnode_t;
+//-----------------------------------------------------------------
+menu_t menu() {
+  menu_t choice = CH_DELETE_END;
+  do {
+    printf("Enter your choice:\n");
+    printf("\t%u for Create SLL\n", CH_CREATE);
+    printf("\t%u for Display\n", CH_DISPLAY);
+    printf("\t%u for Insert at front\n", CH_INSERT_FRONT);
+    printf("\t%u for Delete at front\n", CH_DELETE_FRONT);
+    printf("\t%u for Insert at end\n", CH_INSERT_END);
+    printf("\t%u for Delete at end\n", CH_DELETE_END);
+    printf("\t%u for Exit.\n", CH_EXIT);
+    printf("\tchoice: ");
+    scanf("%u", & choice);
+  } while (choice >CH_DELETE_END);
+  getchar();
+  return choice;
 }
-
-void insert_atfirst() { 
-  if (first == NULL) { 
-    create(); 
-    first = temp; 
-    last = first; 
+//-----------------------------------------------------------------
+listnode_t* create(listnode_t* node) {
+  if(node == NULL) {
+    node = malloc(sizeof(listnode_t));
+    printf("\n\tEnter USN of the student: ");scanf("%s", node->USN);
+    printf("\n\tEnter the name of the student: ");scanf("%s", node->name);
+    printf("\n\tEnter the programme of the student: ");scanf("%s", node->programme);
+    printf("\n\tEnter the sem of the student studing at present: ");scanf("%s", node->sem);
+    printf("\n\tEnter the contact number of the student: ");scanf("%s", node->phone_no);
+    node->next = NULL;
   }
-  else{ 
-    create(); 
-    temp->next = first; 
-    first = temp; 
-  } 
+  return node;
+}
+//-----------------------------------------------------------------
+void display(listnode_t* node) {
+  listnode_t* temp = node;
+  int num = 1;
+  while(temp!=NULL) {
+    printf("\n  student %d \n", num);
+    printf("\tUSN: %s\n", temp->USN);
+    printf("\tName: %s\n", temp->name);
+    printf("\tProgramme: %s\n", temp->programme);
+    printf("\tSem: %s\n", temp->sem);
+    printf("\tPhone Number: %s\n", temp->phone_no);
+    temp = temp->next;
+    num++;
+  }
+}
+//-----------------------------------------------------------------
+listnode_t* insert_end(listnode_t* node) {
+  listnode_t* temp = node;
+  listnode_t* end = NULL;
+  while(temp->next != NULL) {
+    temp = temp->next;
+  }
+  end = malloc(sizeof(listnode_t));
+  printf("\n\tEnter USN of the student: ");scanf("%s", end->USN);
+  printf("\n\tEnter the name of the student: ");scanf("%s", end->name);
+  printf("\n\tEnter the programme of the student: ");scanf("%s", end->programme);
+  printf("\n\tEnter the sem of the student studing at present: ");scanf("%s", end->sem);
+  printf("\n\tEnter the contact number of the student: ");scanf("%s", end->phone_no);
+  end->next = NULL;
+  temp->next = end;
+  return node;
+}
+//-----------------------------------------------------------------
+listnode_t* insert_front(listnode_t* node) {
+  listnode_t* temp = NULL;
+  temp = malloc(sizeof(listnode_t));
+  printf("\n\tEnter USN of the student: ");scanf("%s", temp->USN);
+  printf("\n\tEnter the name of the student: ");scanf("%s", temp->name);
+  printf("\n\tEnter the programme of the student: ");scanf("%s", temp->programme);
+  printf("\n\tEnter the sem of the student studing at present: ");scanf("%s", temp->sem);
+  printf("\n\tEnter the contact number of the student: ");scanf("%s", temp->phone_no);
+  temp->next = node;
+  node = temp;
+  return node;
+}
+//-----------------------------------------------------------------
+listnode_t* delete_front(listnode_t* node) {
+  listnode_t* ptr;
+  ptr = node;
+  node = node->next;
+  free(ptr);
+  return node;
+}
+//-----------------------------------------------------------------
+listnode_t* delete_end(listnode_t* node) {
+  listnode_t* cur = node;
+ listnode_t* del;
+    
+  while(cur->next->next != NULL) {
+    cur = cur->next;
+  }
+  del = cur->next;
+  cur->next = NULL;
+  free(del);
+  cur = node;
+  return node;
+}
+//-----------------------------------------------------------------
+void fn_exit(listnode_t* node) {
+  free(node);
+  exit(0);
+}
+//-----------------------------------------------------------------
+int main(int argc, char* argv[]) {
+  listnode_t* node = NULL;
+  menu_t choice;
+
+  while (1) {
+    choice = menu();
+      switch (choice) {
+        case CH_CREATE:
+          if(node != NULL) {
+            printf("\n\tLinked list already created\n");
+            continue;
+          }
+          node = create(node);
+          break;
+
+        case CH_DISPLAY:
+          if(node != NULL) {
+            display(node);
+            continue;
+          }
+          printf("\t\tLinked list not created\n");
+          break;
+
+        case CH_INSERT_FRONT:
+          if(node == NULL) {
+            printf("\n\tLinked list not created\n");
+            continue;
+          }
+          node  = insert_front(node);
+          printf("\n\tNode inserted at front\n");
+          break;
+
+        case CH_INSERT_END:
+          if(node == NULL) {
+            printf("\n\tLinked list not created\n");
+            continue;
+          }
+          node  = insert_end(node);
+          printf("\n\tNode inserted at end\n");
+          break;
+
+        case CH_DELETE_FRONT:
+          if(node == NULL){
+            printf("\n\tLinked list not created\n");
+            continue;
+          }
+          node = delete_front(node);
+          printf("\n\tNode deleted at front\n");
+          break;
+
+        case CH_DELETE_END:
+          if(node == NULL){
+            printf("\n\tLinked list not created\n");
+            continue;
+          }
+          node = delete_end(node);
+          printf("\n\tNode deleted at end\n");
+          break;
+
+        case CH_EXIT:
+        default:
+          fn_exit(node);
+          break;
+    }
+  }
 }
 
-void insert_atlast() { 
-  if(first==NULL) { 
-    create(); 
-    first = temp; 
-    last = first; 
-  } 
-  else { 
-    create(); 
-    last->next = temp; 
-    last = temp; 
-  } 
-}
-
-void display() { 
-  temp1=first; 
-  if(temp1 == NULL) { 
-    printf("List empty to display \n"); 
-    return; 
-  } 
-  printf("\n Linked list elements from begining : \n"); 
-  while (temp1!= NULL) { 
-    printf("%s %s %s %d %d\n", temp1->usn, temp1->name,temp1->branch, 
-    temp1->sem,temp1->phno ); 
-    temp1 = temp1->next; 
-  } 
-  printf(" No of students = %d ", count); 
-}
-
-int deleteend() { 
-  struct node *temp; 
-  temp=first; 
-  if(temp->next==NULL) { 
-    free(temp); 
-    first=NULL; 
-  } 
-  else { 
-    while(temp->next!=last) 
-      temp=temp->next; 
-      printf("%s %s %s %d %d\n", last->usn, last->name,last->branch, last->sem, 
-      last->phno ); 
-      free(last); 
-      temp->next=NULL; 
-      last=temp; 
-    } 
-    count--; 
-  return 0; 
-}
-
-int deletefront() { 
-  struct node *temp; 
-  temp=first; 
-  if(temp->next==NULL) { 
-    free(temp); 
-    first=NULL; 
-    return 0; 
-  } 
-  else { 
-    first=temp->next; 
-    printf("%s %s %s %d %d", temp->usn, temp->name,temp->branch,temp->sem, 
-    temp->phno ); 
-    free(temp); 
-  } 
-  count--; 
-  return 0; 
-}
-
-void main() { 
-  int ch,n,i; 
-  first=NULL; 
-  temp = temp1 = NULL; 
-  printf("-----------------MENU----------------------\n");
-  printf("\n 1- create a SLL of n emp");
-  printf("\n 2 - Display from beginning"); 
-  printf("\n 3 - Insert at end");
-  printf("\n 4 - delete at end");
-  printf("\n 5 - Insert at beg");
-  printf("\n 6 - delete at beg"); 
-  printf("\n 7 - exit\n");
-  printf("-------------------------------------------\n"); 
-  while (1) { 
-    printf("\n Enter choice : "); 
-    scanf("%d", &ch); 
-    switch (ch) { 
-      case 1: 
-        printf("\n Enter no of students : "); 
-        scanf("%d", &n); 
-        for(i=0;i<n;i++) 
-        insert_atfirst(); 
-        break; 
-      case 2: 
-        display(); 
-        break; 
-      case 3: 
-        insert_atlast(); 
-        break; 
-      case 4: 
-        deleteend(); 
-        break; 
-      case 5: 
-        insert_atfirst(); 
-        break; 
-      case 6: 
-        deletefront(); 
-        break; 
-      case 7: 
-        exit(0); 
-      default: 
-        printf("wrong choice\n");
-    } 
-  } 
-} 
